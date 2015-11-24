9f45c44d1f55c2ca43f5c11e139d5fe47a294735
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2015-11-24 13:11:20 +0000

    Add Objective-C version of Lab 6.

---
+++ 06/objc/truck.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+#import "vehicle.h"
+
+@interface Truck : Vehicle
+
+@property (nonatomic, getter=ifLoaded) BOOL load;
+
+- (id) AmountOfDoor: (int) door AmountOfCylinder: (int) cylinder TransmissionType: (int) trans Color: (NSString *) color FuelLevel: (double) fuel IfLoaded: (BOOL) load;
+- (void) printDetail;
+- (void) horn;
+
+@end
